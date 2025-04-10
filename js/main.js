1154e732c13444d0eb3451af8a8472ab6c6f7544
Nam Le <hirnam.2024@gmail.com>
2025-04-10 20:45:02 +0700

    Nam Le 2

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
         $('.btn-play').click(function () {
             $videoSrc = $(this).data("src");
         });
-        console.log($videoSrc);
+        
 
         $('#videoModal').on('shown.bs.modal', function (e) {
             $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
@@ -108,4 +108,48 @@
     });
 
 })(jQuery);
+const form = document.querySelector('form[class="dat-lich"]')
+if(form){
+    console.log(form)
+    const country=form.querySelector('select.country')
+    const city=form.querySelector('select.city')
+    const area=form.querySelector('select.area')
+    const event=form.querySelector('select.event')
+    const clients=form.querySelector('select.clients')
+    const phone=form.querySelector('input.phone')
+    const date=form.querySelector('input.date')
+    const email=form.querySelector('input.email')
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        // console.log(country.value)
+        // console.log(city.value)
+        // console.log(area.value)
+        // console.log(event.value)
+        // console.log(clients.value)
+        // console.log(phone.value)
+        // console.log(email.value)
+        // console.log(date.value)
+        const alertPlaceholder = document.querySelector('#liveAlertPlaceholder')
 
+        const appendAlert = (message, type) => {
+            const wrapper = document.createElement('div')
+            wrapper.innerHTML = [
+              `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+              `   <div>${message}</div>`,
+              '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+              '</div>'
+            ].join('')
+          
+            alertPlaceholder.append(wrapper)
+          }
+        if(city.value!='default'&&country.value!='default'&&area.value!='default'&&event.value!='default'&&clients.value!='default'&&date.value!=""&&email!=""&&phone!=""){
+            form.submit()
+            appendAlert('Đặt lịch thành công', 'success')
+            
+        }
+        else{
+            appendAlert('Vui lòng diền đủ thông tin', 'danger')
+        }
+    })
+
+}
